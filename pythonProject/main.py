5328cfa8f478d95dfd627509c9d95f2b420d3b2f
Rison <slavadurasov@yandex.ru>
2024-09-09 22:24:48 +0500

    Пузырь 2.0

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,8 +1,19 @@
+def puzir(n, x):
+    if x == 1:
+        for i in range(len(n) - 1):
+            for j in range(len(n) - 1 - i):
+                if n[j] > n[j + 1]:
+                    n[j], n[j + 1] = n[j + 1], n[j]
+        return n
+    if x == 2:
+        for i in range(len(n) - 1):
+            for j in range(len(n) - 1 - i):
+                if n[j] < n[j + 1]:
+                    n[j], n[j + 1] = n[j + 1], n[j]
+        return n
+
 n = input("Введите числа: ").split()
 n = [int(num) for num in n]
+x = int(input("По возрастанию - 1 или по убыванию - 2: "))
 print("Изначальный массив:\n", n)
-for i in range(len(n)-1):
-    for j in range(len(n)-1-i):
-        if n[j] > n[j+1]:
-            n[j], n[j+1] = n[j+1], n[j]
-print("Сортировка пузырьком:\n", n)
+print("Сортировка пузырьком:\n", puzir(n,x))
